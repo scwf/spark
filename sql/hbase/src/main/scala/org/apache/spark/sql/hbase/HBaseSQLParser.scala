910a5c07d98a4368d07177cd381f734c7c84ab17
xinyunh <xinyun.huang@huawei.com>
2014-09-04 16:05:12 -0700

    add CREATE and DROP

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseSQLParser.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseSQLParser.scala
@@ -10,10 +10,47 @@ import org.apache.spark.sql.catalyst.types._
 
 class HBaseSQLParser extends SqlParser{
   protected val CREATE = Keyword("CREATE")
+  protected val DROP = Keyword("DROP")
+  protected val ALTER = Keyword("ALTER")
+  protected val EXISTS = Keyword("EXISTS")
+  protected val MAPPED = Keyword("MAPPED")
 
   protected lazy val create: Parser[LogicalPlan] =
-    CREATE ~> opt(DISTINCT)  <~ opt(";") ^^ {
-      //to-do
-      null
+    CREATE ~> TABLE ~> opt(IF ~ NOT ~ EXISTS ^^^ true) ~ ident ~ ("(" ~> tableCols <~ ")") ~ (MAPPED ~> BY ~> "(" ~> ident <~ ",") ~ colFamilies <~ ")" <~ opt(";") ^^ {
+      case i ~ t ~ r ~ a ~ b=>
+        println("\nin Create")
+        println(i)
+        println(t)
+        println(r)
+        println(a)
+        println(b)
+        null
     }
+
+  protected lazy val drop: Parser[LogicalPlan] =
+    DROP ~> TABLE ~> ident <~ opt(";") ^^ {
+      case t =>
+        println("\nin Drop")
+        println(t)
+        null
+    }
+
+  protected lazy val colFamily: Parser[Expression] = expression ^^ {case e => e}
+
+  protected lazy val colFamilies: Parser[Seq[Expression]] = repsep(colFamily, ",")
+
+  protected lazy val tableCol: Parser[Expression] =
+    expression ~ (expression | STRING)  ^^ {
+      case e1 ~ e2 => Alias(e1, e2.toString)()
+    }
+
+  protected lazy val tableCols: Parser[Seq[Expression]] = repsep(tableCol, ",")
+
+  protected lazy val alter: Parser[LogicalPlan] =
+    ALTER ~> opt(OVERWRITE) ~ inTo ~ select <~ opt(";") ^^ {
+      case o ~ r ~ s =>
+        val overwrite: Boolean = o.getOrElse("") == "OVERWRITE"
+        InsertIntoTable(r, Map[String, Option[String]](), s, overwrite)
+    }
+
 }
