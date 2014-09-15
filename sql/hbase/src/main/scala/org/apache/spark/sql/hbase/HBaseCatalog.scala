576e98b87e38f836fb71825b9e52dc3a525fc9b9
Bo Meng <bo.meng@huawei.com>
2014-09-15 16:07:24 -0700

    add create table support

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseCatalog.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseCatalog.scala
@@ -17,13 +17,15 @@
 package org.apache.spark.sql.hbase
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.client.{HTable, HBaseAdmin, HConnectionManager, HTableInterface}
+import org.apache.hadoop.hbase.client.{Get, HBaseAdmin, HConnectionManager, HTable, HTableInterface, Put}
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, TableName}
+import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
 import org.apache.log4j.Logger
 import org.apache.spark.Logging
 import org.apache.spark.sql.catalyst.analysis.Catalog
 import org.apache.spark.sql.catalyst.plans.logical._
+
+import scala.collection.mutable.{HashMap, LinkedHashMap, ListBuffer}
 
 /**
  * HBaseCatalog
@@ -35,6 +37,14 @@ private[hbase] class HBaseCatalog(hbaseContext: HBaseSQLContext) extends Catalog
     val connection = HConnectionManager.createConnection(configuration)
     connection
   }
+
+  val METADATA = "metadata"
+  val COLUMN_FAMILY = Bytes.toBytes("colfam")
+  val QUAL_KEYS = Bytes.toBytes("keys")
+  val QUAL_COLUMN_INFO = Bytes.toBytes("columnInfo")
+  val QUAL_HBASE_NAME = Bytes.toBytes("hbaseName")
+  val QUAL_MAPPING_INFO = Bytes.toBytes("mappingInfo")
+  val tables = new HashMap[String, LogicalPlan]()
   val logger = Logger.getLogger(getClass.getName)
   val caseSensitive: Boolean = false
 
@@ -63,21 +73,114 @@ private[hbase] class HBaseCatalog(hbaseContext: HBaseSQLContext) extends Catalog
     }
   }
 
-  def createTable(tableName: String, columnFamily: String): Unit = {
-    val admin = new HBaseAdmin(hbaseConnection)
-    val descriptor = new HTableDescriptor(TableName.valueOf(tableName))
+  def createTable(dbName: String, tableName: String, columnInfo: LinkedHashMap[String, String], hbaseTableName: String, keys: List[String], mappingInfo: LinkedHashMap[String, String]): Unit = {
+    val conf = HBaseConfiguration.create()
 
-    val columnDescriptor = new HColumnDescriptor(Bytes.toBytes(columnFamily))
-    descriptor.addFamily(columnDescriptor)
+    val admin = new HBaseAdmin(conf)
 
-    admin.createTable(descriptor)
+    val avail = admin.isTableAvailable(METADATA)
+
+    if (!avail) {
+      // create table
+      val desc = new HTableDescriptor(TableName.valueOf(METADATA))
+      val coldef = new HColumnDescriptor(COLUMN_FAMILY)
+      desc.addFamily(coldef)
+      admin.createTable(desc)
+    }
+
+    val table = new HTable(conf, METADATA)
+    table.setAutoFlushTo(false)
+    val rowKey = dbName + "." + tableName
+
+    val get = new Get(Bytes.toBytes(rowKey))
+    if (table.exists(get)) {
+      throw new Exception("row key exists")
+    }
+    else {
+      val put = new Put(Bytes.toBytes(rowKey))
+
+      val result1 = new StringBuilder
+      for ((key, value) <- columnInfo) {
+        result1.append(key)
+        result1.append("=")
+        result1.append(value)
+        result1.append(",")
+      }
+      put.add(COLUMN_FAMILY, QUAL_COLUMN_INFO, Bytes.toBytes(result1.toString))
+
+      val result2 = new StringBuilder
+      result2.append(hbaseTableName)
+      put.add(COLUMN_FAMILY, QUAL_HBASE_NAME, Bytes.toBytes(result2.toString))
+
+      val result3 = new StringBuilder
+      for ((key, value) <- mappingInfo) {
+        result3.append(key)
+        result3.append("=")
+        result3.append(value)
+        result3.append(",")
+      }
+      put.add(COLUMN_FAMILY, QUAL_MAPPING_INFO, Bytes.toBytes(result3.toString))
+
+      val result4 = new StringBuilder
+      for (key <- keys) {
+        result4.append(key)
+        result4.append(",")
+      }
+      put.add(COLUMN_FAMILY, QUAL_KEYS, Bytes.toBytes(result4.toString))
+
+      table.put(put)
+
+      table.flushCommits()
+    }
   }
 
-  def deleteTable(tableName: String): Unit = {
-    val admin = new HBaseAdmin(hbaseConnection)
+  def retrieveTable(dbName: String, tableName: String): (LinkedHashMap[String, String], String, List[String], LinkedHashMap[String, String]) = {
+    val conf = HBaseConfiguration.create()
 
-    admin.disableTable(tableName)
-    admin.deleteTable(tableName)
+    val table = new HTable(conf, METADATA)
+
+    val get = new Get(Bytes.toBytes(dbName + "." + tableName))
+    val rest1 = table.get(get)
+
+    var columnInfo = Bytes.toString(rest1.getValue(COLUMN_FAMILY, QUAL_COLUMN_INFO))
+    if (columnInfo.length > 0) {
+      columnInfo = columnInfo.substring(0, columnInfo.length - 1)
+    }
+    val columnInfoArray = columnInfo.split(",")
+    val columnInfoMap = new LinkedHashMap[String, String]
+    for (column <- columnInfoArray) {
+      val index = column.indexOf("=")
+      val key = column.substring(0, index)
+      val value = column.substring(index + 1)
+      columnInfoMap.put(key, value)
+    }
+
+    val hbaseName = Bytes.toString(rest1.getValue(COLUMN_FAMILY, QUAL_HBASE_NAME))
+
+    var mappingInfo = Bytes.toString(rest1.getValue(COLUMN_FAMILY, QUAL_MAPPING_INFO))
+    if (mappingInfo.length > 0) {
+      mappingInfo = mappingInfo.substring(0, mappingInfo.length - 1)
+    }
+    val mappingInfoArray = mappingInfo.split(",")
+    val mappingInfoMap = new LinkedHashMap[String, String]
+    for (mapping <- mappingInfoArray) {
+      val index = mapping.indexOf("=")
+      val key = mapping.substring(0, index)
+      val value = mapping.substring(index + 1)
+      mappingInfoMap.put(key, value)
+    }
+
+    var keys = Bytes.toString(rest1.getValue(COLUMN_FAMILY, QUAL_KEYS))
+    if (keys.length > 0) {
+      keys = keys.substring(0, keys.length - 1)
+    }
+    val keysArray = keys.split(",")
+    var keysList = new ListBuffer[String]()
+    for (key <- keysArray) {
+      keysList += key
+    }
+
+    (columnInfoMap, hbaseName, keysList.toList, mappingInfoMap)
   }
 
   override def registerTable(databaseName: Option[String], tableName: String,
@@ -88,7 +191,7 @@ private[hbase] class HBaseCatalog(hbaseContext: HBaseSQLContext) extends Catalog
 
   class Columns(val columns: Seq[Column]) {
 
-    import collection.mutable
+    import scala.collection.mutable
 
     val colsMap = columns.foldLeft(mutable.Map[String, Column]()) { case (m, c) =>
       m(s"$c.cf:$c.cq") = c
