a4a810cf53154b0a7be007c3aec0e19a368fc10b
scwf <wangfei1@huawei.com>
2014-10-27 00:21:39 -0700

    new package execution and logical

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseStrategies.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseStrategies.scala
@@ -23,9 +23,10 @@ import org.apache.hadoop.hbase.filter.{Filter => HFilter}
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.planning.{PhysicalOperation, QueryPlanner}
 import org.apache.spark.sql.catalyst.plans.logical
-import org.apache.spark.sql.catalyst.plans.logical.{Filter, Join, LogicalPlan}
+import org.apache.spark.sql.catalyst.plans.logical.{InsertIntoTable, Filter, Join, LogicalPlan}
 import org.apache.spark.sql.execution._
 import org.apache.spark.sql.{SQLContext, SchemaRDD}
+import org.apache.spark.sql.hbase.execution.{DropHbaseTableCommand, CreateHBaseTableCommand, InsertIntoHBaseTable, HBaseSQLTableScan}
 
 /**
  * HBaseStrategies
@@ -91,16 +92,16 @@ private[hbase] trait HBaseStrategies extends QueryPlanner[SparkPlan] {
 
   object HBaseOperations extends Strategy {
     def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
-      case CreateHBaseTablePlan(
+      case logical.CreateHBaseTablePlan(
       tableName, nameSpace, hbaseTableName,
       colsSeq, keyCols, nonKeyCols) =>
         Seq(CreateHBaseTableCommand(
           tableName, nameSpace, hbaseTableName,
           colsSeq, keyCols, nonKeyCols)
           (hbaseSQLContext))
-      case logical.InsertIntoTable(table: HBaseRelation, partition, child, _) =>
+      case InsertIntoTable(table: HBaseRelation, partition, child, _) =>
         new InsertIntoHBaseTable(table, planLater(child))(hbaseSQLContext) :: Nil
-      case DropTablePlan(tableName) => Seq(DropHbaseTableCommand(tableName)(hbaseSQLContext))
+      case logical.DropTablePlan(tableName) => Seq(DropHbaseTableCommand(tableName)(hbaseSQLContext))
       case _ => Nil
     }
   }
--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/logical/HBaseLogicalPlans.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/logical/HBaseLogicalPlans.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.sql.hbase
+package org.apache.spark.sql.hbase.logical
 
 import org.apache.spark.sql.catalyst.plans.logical.Command
 
