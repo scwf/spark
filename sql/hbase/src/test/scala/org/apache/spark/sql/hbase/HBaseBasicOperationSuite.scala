048597f881892ba61eb2c2787d52bd44c1105a80
xinyunh <xinyun.huang@huawei.com>
2014-11-10 10:55:58 -0800

    Use of primary key in DDL

--- sql/hbase/src/test/scala/org/apache/spark/sql/hbase/HBaseBasicOperationSuite.scala
+++ sql/hbase/src/test/scala/org/apache/spark/sql/hbase/HBaseBasicOperationSuite.scala
@@ -29,8 +29,8 @@ class HBaseBasicOperationSuite extends QueryTest {
 
   test("create table") {
     sql( """CREATE TABLE tableName (col1 STRING, col2 BYTE, col3 SHORT, col4 INTEGER,
-      col5 LONG, col6 FLOAT, col7 DOUBLE)
-      MAPPED BY (hbaseTableName1, KEYS=[col7, col1, col3], COLS=[col2=cf1.cq11,
+      col5 LONG, col6 FLOAT, col7 DOUBLE, PRIMARY KEY(col7, col1, col3))
+      MAPPED BY (hbaseTableName1, COLS=[col2=cf1.cq11,
       col4=cf1.cq12, col5=cf2.cq21, col6=cf2.cq22])"""
     )
   }
