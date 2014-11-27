f79665d9e3f16afd7ae051eaa54f468cc39e2d23
sboeschhuawei <stephen.boesch@huawei.com>
2014-11-26 19:46:22 -0800

    Added CreateTableAndLoadData trait

---
+++ sql/hbase/src/test/scala/org/apache/spark/sql/hbase/BasicQueriesSuite.scala
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hbase
+
+import org.scalatest.{BeforeAndAfterEach, ConfigMap, FunSuiteLike}
+
+class BasicQueriesSuite extends HBaseIntegrationTestBase with CreateTableAndLoadData {
+  self : HBaseIntegrationTestBase =>
+
+ override protected def beforeAll(configMap: ConfigMap): Unit = {
+   super.beforeAll(configMap)
+    createTableAndLoadData(hbc)
+  }
+
+  val tabName = DefaultTableName
+  
+  test("StarOperator * with limit") {
+    val query1 =
+      s"""select * from $tabName limit 3"""
+        .stripMargin
+
+    val execQuery1 = hbc.executeSql(query1)
+    val result1 = execQuery1.toRdd.collect()
+    assert(result1.size == 3)
+    assert(result1.mkString(",").equals("[row4,4,8],[row5,5,10],[row6,6,12]"))
+    println(s"Select * from $tabName limit 3 came back with ${result1.size} results")
+    println(result1.mkString)
+
+    val sql2 =
+      s"""select * from $tabName limit 2"""
+        .stripMargin
+
+    val executeSql2 = hbc.executeSql(sql2)
+    val results = executeSql2.toRdd.collect()
+    println(s"Select * from $tabName limit 2 came back with ${results.size} results")
+    assert(results.size == 2)
+    assert(results.mkString(",").equals("[row4,4,8],[row5,5,10]"))
+    println(results.mkString)
+
+    println("Test load data into HBase completed successfully")
+  }
+
+  test("All fields * query with limit") {
+    val query1 =
+      s"""select * from $tabName limit 3"""
+        .stripMargin
+
+    val execQuery1 = hbc.executeSql(query1)
+    val result1 = execQuery1.toRdd.collect()
+    assert(result1.size == 3)
+    assert(result1.mkString(",").equals("[row4,4,8],[row5,5,10],[row6,6,12]"))
+    println(s"Select * from $tabName limit 3 came back with ${result1.size} results")
+    println(result1.mkString)
+
+    val sql2 =
+      s"""select * from $tabName limit 2"""
+        .stripMargin
+
+    val executeSql2 = hbc.executeSql(sql2)
+    val results = executeSql2.toRdd.collect()
+    println(s"Select * from $tabName limit 2 came back with ${results.size} results")
+    assert(results.size == 2)
+    assert(results.mkString(",").equals("[row4,4,8],[row5,5,10]"))
+    println(results.mkString)
+
+    println("Test load data into HBase completed successfully")
+  }
+
+}
---
+++ sql/hbase/src/test/scala/org/apache/spark/sql/hbase/CreateTableAndLoadData.scala
@@ -0,0 +1,100 @@
+package org.apache.spark.sql.hbase
+
+import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor}
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.log4j.Logger
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * CreateTableAndLoadData
+ *
+ */
+trait CreateTableAndLoadData {
+
+  private val logger = Logger.getLogger(getClass.getName)
+  val DefaultStagingTableName = "StageTable"
+  val DefaultTableName = "TestTable"
+  val DefaultHbaseStagingTabName = "stageTab"
+  val DefaultHbaseTabName = "testTab"
+  val DefaultHbaseColFamiles = Seq("cf1","cf2")
+  val DefaultLoadFile = "./sql/hbase/src/test/resources/testTable.csv"
+
+  def createTableAndLoadData(hbc: HBaseSQLContext) = {
+    createTables(hbc)
+    loadData(hbc)
+  }
+
+  def createNativeHbaseTable(hbc: HBaseSQLContext, tableName: String, families: Seq[String]) = {
+    val hbaseAdmin = hbc.catalog.hBaseAdmin
+    val hdesc = new HTableDescriptor(tableName)
+    families.foreach{ f => hdesc.addFamily(new HColumnDescriptor(f))}
+    hbaseAdmin.createTable(hdesc)
+  }
+
+  def createTables(hbc: HBaseSQLContext, stagingTableName: String = DefaultStagingTableName, tableName: String = DefaultTableName) = {
+    // this need to local test with hbase, so here to ignore this
+
+    val hbaseAdmin = hbc.catalog.hBaseAdmin
+    createNativeHbaseTable(hbc, DefaultHbaseStagingTabName,DefaultHbaseColFamiles)
+    createNativeHbaseTable(hbc, DefaultHbaseTabName,DefaultHbaseColFamiles)
+
+    val hbaseStagingSql = s"""create '$DefaultHbaseStagingTabName',['cf1','cf2']"""
+    val hbaseSql = s"""create '$DefaultHbaseTabName',['cf1','cf2']"""
+    var sql1 =
+      s"""CREATE TABLE $stagingTableName(strcol STRING, bytecol String, shortcol String, intcol String,
+          longcol string, floatcol string, doublecol string, PRIMARY KEY(doublecol, strcol, intcol))
+          MAPPED BY ($DefaultHbaseStagingTabName, COLS=[bytecol=cf1.hbytecol,
+          shortcol=cf1.hshortcol, longcol=cf2.hlongcol, floatcol=cf2.hfloatcol])"""
+        .stripMargin
+
+    var executeSql1 = hbc.executeSql(sql1)
+    executeSql1.toRdd.collect().foreach(println)
+
+        logger.debug(s"Created table $tableName: " +
+      s"isTableAvailable= ${hbaseAdmin.isTableAvailable(s2b(DefaultHbaseStagingTabName))}" +
+      s" tableDescriptor= ${hbaseAdmin.getTableDescriptor(s2b(DefaultHbaseStagingTabName))}")
+
+    sql1 =
+      s"""CREATE TABLE $tableName(strcol STRING, bytecol BYTE, shortcol SHORT, intcol INTEGER,
+          longcol LONG, floatcol FLOAT, doublecol DOUBLE, PRIMARY KEY(doublecol, strcol, intcol))
+          MAPPED BY ($DefaultHbaseTabName, COLS=[bytecol=cf1.hbytecol,
+          shortcol=cf1.hshortcol, longcol=cf2.hlongcol, floatcol=cf2.hfloatcol])"""
+        .stripMargin
+
+    executeSql1 = hbc.executeSql(sql1)
+    executeSql1.toRdd.collect().foreach(println)
+
+  }
+
+  def loadData(hbc: HBaseSQLContext, tableName: String = DefaultTableName,
+               loadFile: String = DefaultLoadFile) = {
+    // then load data into table
+    val hbaseAdmin = hbc.catalog.hBaseAdmin
+    val loadSql = s"LOAD DATA LOCAL INPATH '$loadFile' INTO TABLE $tableName"
+    val result3 = hbc.executeSql(loadSql).toRdd.collect()
+    val insertSql = s"""insert into $tableName select cast(strcol as string),
+    cast(bytecol as tinyint), cast(shortcol as smallint), cast(intcol as int),
+    cast (longcol as bigint), cast(floatcol as float), cast(doublecol as double)
+    from $DefaultHbaseStagingTabName"""
+  }
+
+  def s2b(s: String) = Bytes.toBytes(s)
+
+}
