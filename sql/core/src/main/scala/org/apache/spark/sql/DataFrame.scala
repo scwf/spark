60ede9c5f69907d3f0bf0ed0cf430ff8790aeb92
scwf <wangfei1@huawei.com>
2015-04-19 20:38:34 +0800

    fix twice execute

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrame.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrame.scala
@@ -140,8 +140,9 @@ class DataFrame private[sql](
     // For various commands (like DDL) and queries with side effects, we force query optimization to
     // happen right away to let these side effects take place eagerly.
     case _ : Command =>
-      queryExecution.sparkPlan.executeCollect()
-      queryExecution.analyzed
+      LocalRelation(
+        queryExecution.analyzed.output,
+        queryExecution.sparkPlan.executeCollect())
     case _: InsertIntoTable |
       _: CreateTableAsSelect[_] |
       _: CreateTableUsingAsSelect |
